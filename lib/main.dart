da5bcbb55310c0c8621b55ce77f3cd608a6983c3
rldnd <gi981226@gmail.com>
2023-01-29 13:47:21 +0900

    chore: init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
