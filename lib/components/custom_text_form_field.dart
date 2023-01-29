4c5b1dab8871e6b07e520d6c0bbe14964af36e0d
rldnd <gi981226@gmail.com>
2023-01-29 14:37:56 +0900

    feat(pkg): customTextFormField
    add default colors in customTextFormField
    and set parmeters
    
    Resolving: #1

---
+++ lib/components/custom_text_form_field.dart
@@ -0,0 +1,53 @@
+import 'package:delivery/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? errorText;
+  final String hintText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    required this.onChanged,
+    super.key,
+    this.errorText,
+    this.hintText = '입력해주세요',
+    this.obscureText = false,
+    this.autofocus = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: C_INPUT_BORDER,
+        width: 1.0,
+      ),
+    );
+
+    return TextFormField(
+      cursorColor: C_PRIMARY,
+      onChanged: onChanged,
+      autofocus: autofocus,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(20.0),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: const TextStyle(
+          color: C_BODY_TEXT,
+          fontSize: 14.0,
+        ),
+        fillColor: C_INPUT_BG,
+        filled: true,
+        border: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: C_PRIMARY,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:delivery/components/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,24 @@ class _App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: Container()));
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요.',
+              onChanged: (value) {},
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요.',
+              obscureText: true,
+              onChanged: (value) {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
