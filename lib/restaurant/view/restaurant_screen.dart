5901008abcda9ca73a48d8ee6163bc062832396b
rldnd <gi981226@gmail.com>
2023-02-12 22:52:27 +0900

    reset

---
+++ lib/restaurant/view/restaurant_screen.dart
@@ -0,0 +1,80 @@
+import 'package:actual/common/const/data.dart';
+import 'package:actual/common/dio/dio.dart';
+import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  const RestaurantScreen({Key? key}) : super(key: key);
+
+  Future<List> paginateRestaurant() async {
+    final dio = Dio();
+
+    dio.interceptors.add(
+      CustomInterceptor(storage: storage),
+    );
+
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      'http://$ip/restaurant',
+      options: Options(
+        headers: {
+          'authorization': 'Bearer $accessToken',
+        },
+      ),
+    );
+
+    return resp.data['data'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: FutureBuilder<List>(
+              future: paginateRestaurant(),
+              builder: (context, AsyncSnapshot<List> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                return ListView.separated(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (_, index) {
+                    final item = snapshot.data![index];
+                    final pItem = RestaurantModel.fromJson(
+                      item,
+                    );
+
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => RestaurantDetailScreen(
+                              id: pItem.id,
+                            ),
+                          ),
+                        );
+                      },
+                      child: RestaurantCard.fromModel(
+                        model: pItem,
+                      ),
+                    );
+                  },
+                  separatorBuilder: (_, index) {
+                    return SizedBox(height: 16.0);
+                  },
+                );
+              },
+            )),
+      ),
+    );
+  }
+}
