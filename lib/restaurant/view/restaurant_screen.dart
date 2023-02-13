5e50edf2672f4440540a55780bf28a62e2b8c4d6
rldnd <gi981226@gmail.com>
2023-02-13 20:27:57 +0900

    feat(restaurant): circularProgressIndicator after last item

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -52,8 +52,20 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
         controller: controller,
-        itemCount: cp.data.length,
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Center(
+                child: data is CursorPaginationFetchingMore
+                    ? const CircularProgressIndicator()
+                    : Text('마지막 데이터 입니다 ㅠ'),
+              ),
+            );
+          }
+
           final pItem = cp.data[index];
 
           return GestureDetector(
